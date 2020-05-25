1946d92460d9b37230b914290dcdaceb10a6c90f
SusiIrwin <susi.irwin@gmail.com>
2020-05-24 22:28:05 -0600

    WIP: Adds Figaro Gem and secret API key. Endpoint can not be seen locally. Need to update test with possible VCR

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'puma', '~> 4.1'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'faraday'
+gem 'figaro'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
@@ -30,7 +31,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.0'
-  gem 'pry-byebug'
+  gem 'pry-byebug'  
 end
 
 group :test do
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,13 +1,11 @@
 class Album < ApplicationRecord
-
-  
   
   def self.find_top_albums_for_artist(artist_name)
     conn = Faraday.new(:url => 'http://ws.audioscrobbler.com/2.0/')
     response = conn.get do |req|                     
       req.params['method'] = "artist.gettopalbums"
       req.params['artist'] = "#{artist_name}"
-      req.params['api_key'] = API_KEY
+      req.params['api_key'] = "#{ENV["LASTFM_API_KEY"]}"
       req.params['format'] = "json"
     end
     parsed = JSON.parse(response.body)
